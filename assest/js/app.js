5ef343f264b277516196ae8273e234b451064693
mhmd mhdi <mhdimhdin5@gmail.com>
2024-11-25 09:20:21 +0330

    add js to nav

---
+++ assest/js/app.js
@@ -0,0 +1,7 @@
+function open__nav__mobile__menu() {
+  document.getElementById('nav__mobile').classList.add("active")
+}
+
+function close__nav__mobile__menu() {
+  document.getElementById('nav__mobile').classList.remove("active")
+}
