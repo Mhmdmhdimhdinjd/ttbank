e847cb958f85e715eb7715e043bac8cddd097da5
mhmd mhdi <mhdimhdin5@gmail.com>
2024-11-25 10:09:45 +0330

    add js to download app

--- assest/js/app.js
+++ assest/js/app.js
@@ -7,3 +7,35 @@ var swiper = new Swiper(".mySwiper", {
   },
 
 });
+
+const androidButton = document.getElementById('section__android');
+const iphoneButton = document.getElementById('section__ios');
+const webappButton = document.getElementById('section__webapp')
+const download__andriod = document.getElementById('section__download__andriod')
+
+androidButton.addEventListener('click', function () {
+  if (!androidButton.getAttribute("class").includes("active")) {
+    androidButton.classList.add('active');
+    download__andriod.style.display = 'flex'
+    iphoneButton.classList.remove('active');
+    webappButton.classList.remove('active');
+  }
+});
+
+iphoneButton.addEventListener('click', function () {
+  if (!iphoneButton.getAttribute("class").includes("active")) {
+    iphoneButton.classList.add('active');
+    download__andriod.style.display = 'none'
+    androidButton.classList.remove('active');
+    webappButton.classList.remove('active');
+  }
+});
+
+webappButton.addEventListener('click', function () {
+  if (!webappButton.getAttribute("class").includes("active")) {
+    webappButton.classList.add('active');
+    download__andriod.style.display = 'none'
+    androidButton.classList.remove('active');
+    iphoneButton.classList.remove('active');
+  }
+});
